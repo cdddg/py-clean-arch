eeb178b4fe1fbdea47fdf88d0bee3793e0f40738
cdddg <cdddg2@gmail.com>
2023-04-15 13:19:33 +0800

    fix(db): support in-memory sqlite (#4)

--- src/tests/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-FIXME: known bug
-  1. `sqlite+aiosqlite:///:memory:` not working (sqlalchemy.exc.OperationalError: (sqlite3.OperationalError) no such table: pokemon)
-  2. `sqlite+aiosqlite:///./src/tests/test.db` savepoint not working
-"""
