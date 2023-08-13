3b7335acaba2f590ee26407a4f828e151f461e54
cdddg <cdddg2@gmail.com>
2023-08-13 11:02:58 +0800

    fix(product): modify unit_or_work to conform to Dependency-Injection (#7)
    * fix(product): modify unit_or_work to conform to Dependency-Injection
    
    * fix(DI): add docstring

--- src/settings/dependency_injection.py
+++ src/settings/dependency_injection.py
@@ -1,3 +1,30 @@
+"""
+Dependency Injection Configuration for the Application.
+
+For specialized or more advanced requirements, consider some of the recommended DI tools within the Python community:
+    - https://github.com/python-injector/injector
+    - https://github.com/ets-labs/python-dependency-injector
+    - https://github.com/ivankorobkov/python-inject
+
+Usage:
+     from dependency_injection import async_unit_of_work
+"""
+
+
+from pkg.repositories.rdbms.pokemon.repository import (
+    EvolutionRepository,
+    PokemonRepository,
+    TypeRepository,
+)
 from settings.unit_of_work import AsyncSqlAlchemyUnitOfWork
 
-async_unit_of_work = AsyncSqlAlchemyUnitOfWork
+
+def create_async_sqlalchemy_uow() -> AsyncSqlAlchemyUnitOfWork:
+    return AsyncSqlAlchemyUnitOfWork(
+        PokemonRepository,
+        TypeRepository,
+        EvolutionRepository,
+    )
+
+
+async_unit_of_work = create_async_sqlalchemy_uow
