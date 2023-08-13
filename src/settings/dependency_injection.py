427c9afb8cf746ab56bceefa69206e56b750715d
cdddg <cdddg2@gmail.com>
2023-08-13 10:58:57 +0800

    fix(DI): add docstring

--- src/settings/dependency_injection.py
+++ src/settings/dependency_injection.py
@@ -1,3 +1,16 @@
+"""
+Dependency Injection Configuration for the Application.
+
+For specialized or more advanced requirements, consider some of the recommended DI tools within the Python community:
+    - https://github.com/python-injector/injector
+    - https://github.com/ets-labs/python-dependency-injector
+    - https://github.com/ivankorobkov/python-inject
+
+Usage:
+     from dependency_injection import async_unit_of_work
+"""
+
+
 from pkg.repositories.rdbms.pokemon.repository import (
     EvolutionRepository,
     PokemonRepository,
