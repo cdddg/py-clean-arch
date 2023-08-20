26724523a554f1150ab70fbd6f0fd2cd4f764c34
cdddg <cdddg2@gmail.com>
2023-08-20 20:49:18 +0800

    test(pytest): add integration and unit testing (#13)

---
+++ src/tests/integration/__init__.py
@@ -0,0 +1,14 @@
+"""
+Integration and Functional Testing for the Application
+
+This module contains the integration and functional tests for the entire application.
+The tests here mainly focus on the interactions between different components and
+layers of the application, ensuring that everything works together as expected.
+
+Key Characteristics:
+    - Integration Tests: Examine the interactions between different modules and layers.
+    - Functional Tests: Ensure the system works as intended from the user's perspective.
+
+Tests are organized based on the features or functionalities they represent, ensuring a
+clear understanding of what is being tested.
+"""
