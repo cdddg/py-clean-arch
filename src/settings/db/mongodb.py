dcfcd53a81870cf9324f747b168e27495223b3ef
Will Lin <39718445+cdddg@users.noreply.github.com>
2024-04-26 22:59:51 +0800

    fix: nosql odm docstring (#34)

--- src/settings/db/mongodb.py
+++ src/settings/db/mongodb.py
@@ -14,3 +14,7 @@ AsyncMongoDBEngine = AsyncIOMotorClient(normalize_uri(DATABASE_URI))
 async def initialize_mongo_db(**kwargs):
     if should_reinitialize(DATABASE_URI):
         await AsyncMongoDBEngine.drop_database(AsyncMongoDBEngine.get_default_database().name)
+
+    db = AsyncMongoDBEngine[DATABASE_NAME]
+    collection = db[COLLECTION_NAME]
+    await collection.create_index([('no', 1)], unique=True)
